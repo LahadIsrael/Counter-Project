14afbc4a200e123e69a1578ca1e336859961bf72
lahad israel <lahad.israel@gmail.com>
2023-02-17 00:55:27 +0000

    Counter-Project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [counter,setCounter] = useState(0)
+	useEffect(()=>{
+		setInterval(()=>{
+			setCounter(currentValue=>currentValue+1)
+		},1000)
+	},[])
+	console.log(counter)
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex gap-5">
+			<p>{Math.floor(counter/100000)%10}</p>
+			<p>{Math.floor(counter/10000)%10}</p>
+			<p>{Math.floor(counter/1000)%10}</p>
+			<p>{Math.floor(counter/100)%10}</p>
+			<p>{Math.floor(counter/10)%10}</p>
+			<p>{counter%10}</p>
+			
 		</div>
 	);
 };
